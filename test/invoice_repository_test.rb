ba55fa9e2b7865689d00ba77adce3c2325a03e52
Larisasmiles <larisasmiles@gmail.com>
2017-06-03 16:04:17 -0600

    invoice-fixtures

--- test/invoice_repository_test.rb
+++ test/invoice_repository_test.rb
@@ -1,5 +1,15 @@
-require 'test_helper'
+require_relative 'test_helper'
+require './lib/invoice_repository'
 
 class InvoiceRepositoryTest < Minitest::Test
-  
+
+  def test_if_it_exists
+    inr = InvoiceRepository.new
+
+    assert_instance_of InvoiceRepository, inr
+  end
+
+  def test_if_it_returns_all_instances_in_array
+    assert_instance_of Array, inr
+  end
 end
